5f6c58a4f126f8210e3660178ad7178c85fc3d3d
Ilia Muratov <i.murato@mail.ru>
2020-11-07 19:46:21 +0400

    Add button

---
+++ Assets/Scripts/UiButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class UiButton : MonoBehaviour
+{
+    private Animator Animator;
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void Push()
+    {
+        Animator.Play("Button-Push");
+    }
+}
