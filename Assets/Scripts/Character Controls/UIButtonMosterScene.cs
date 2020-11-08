91b3a2c6a8d4b6e6d7d27aecca3f4c40a2b58e03
sokerdev <farion1734@gmail.com>
2020-11-08 04:07:32 +0200

    attack buttons to game actions

---
+++ Assets/Scripts/Character Controls/UIButtonMosterScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonMosterScene : MonoBehaviour
+{
+    private Animator Animator;
+    [SerializeField] private Game game;
+    [SerializeField] private Game.ClickType clickType;
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>(); 
+        Debug.Log("GOt ANIMATOR");
+    }
+
+    public void Push()
+    {
+        Animator.Play("Button-Push");
+        game.startClickAction(clickType);        
+    }
+}
